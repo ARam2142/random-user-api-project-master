db3fcdeb2a938ae3e60c8d357755f427f14bc355
Aram2142 <andresaaramirez@aol.com>
2021-04-02 23:28:00 -0400

    erased useless comments

--- js/app.js
+++ js/app.js
@@ -31,24 +31,3 @@ function getData() {
 }
 
 genButton.addEventListener("click", getData)
-
-
-// let output = `
-//             <p class="text-capitalize"><span class="mr-3 text-primary"><i class="fas fa-user"></i></span>first name :<span
-//                   id="first" class="text-uppercase ml-2 badge badge-secondary single-info">${profile.results[0].name.first}</span></p>
-
-//               <p class="text-capitalize"><span class="mr-3 text-primary"><i class="fas fa-user"></i></span>last name
-//                 :<span id="last" class="text-uppercase ml-2 badge badge-secondary single-info">${profile.results[0].name.last}</span></p>
-
-//               <p class="text-capitalize"><span class="mr-3 text-primary"><i
-//                     class="fas fa-search-location"></i></span>location
-//                 :<span id="street" class="text-uppercase ml-2 badge badge-secondary single-info">${profile.results[0].location.city}</span>
-//               </p>
-
-//               <p class="text-capitalize"><span class="mr-3 text-primary"><i class="fas fa-phone"></i></span>phone :<span
-//                   id="phone" class="text-uppercase ml-2 badge badge-secondary single-info">${profile.results[0].phone}</span></p>
-
-//               <p class="text-capitalize"><span class="mr-3 text-primary"><i class="fas fa-envelope"></i></span>email
-//                 :<span id="email" class="text-uppercase ml-2 badge badge-secondary single-info">${profile.results[0].email}</span>
-//               </p>
-//             `;
